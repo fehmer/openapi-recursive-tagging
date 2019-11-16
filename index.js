41d235f99b2a8fc80add3ed8b728901335515502
Christian Fehmer <cfe@sexy-developer.com>
2019-11-16 21:00:16 +0100

    Add support for tags and x-tagGroups

--- index.js
+++ index.js
@@ -3,20 +3,48 @@
 const recurse = require('reftools/lib/recurse.js').recurse;
 
 function process(src, options) {
-    recurse(src,{},function(obj,key,state) {
+
+    recurse(src.paths,{},function(obj,key,state) {
         if(obj["x-public"]) {
-            recurse(obj,{},function(rec,key,state) {
-                if(rec["$ref"]){
-                    let path = rec["$ref"].substring(2).split('/');
-                    let reference = src;
-                    path.forEach(p => reference=reference[p]);
-                    reference["x-public"] = true;
-                }
-            });
+            handle(src, obj);
         }
     });
+
     return src;
 };
+
+function handle(src, obj){
+    recurse(obj,{},function(rec,key,state) {
+        handleTags(src, rec);
+            if(rec["$ref"]){
+                let path = rec["$ref"].substring(2).split('/');
+                let reference = src;
+                path.forEach(p => reference=reference[p]);
+                mark(reference)
+                handle(src, reference);
+            }
+        });
+}
+
+
+function handleTags(src, obj){
+    if(obj.tags){
+        obj.tags.forEach(activeTag => {
+
+            src.tags
+                .filter(tag => tag.name == activeTag)
+                .forEach(tag => mark(tag) );
+
+            src["x-tagGroups"]
+                .filter(group => group.tags.includes(activeTag))
+                .forEach(group => mark(group) );
+        });
+    }
+}
+
+function mark(src){
+    src["x-public"] = true;
+}
 
 module.exports = {
     process : process
